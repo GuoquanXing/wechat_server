82378addfa7d358628c7690897fba241d64e3950
Guoquan <guoquan.xing123@gmail.com>
2018-08-09 13:22:17 +0800

    add new API from tencent

--- handle_work.py
+++ handle_work.py
@@ -4,13 +4,30 @@ import hashlib
 import reply
 import receive
 import web
+from WXBizMsgCrypt import WXBizMsgCrypt
 
 class Handle(object):
 
     def GET(self):
         try:
             data = web.input()
+            if len(data) == 0:
+                return "no paramter given"    
             print data
+            signature = data.msg_signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            token = "hello2018"  # 请按照公众平台官网\基本配置中信息填写
+            
+            wxcpt=WXBizMsgCrypt(token,'Fc72tGGPgLdJHThLJsTNtFa5KgpVL8WFIuR7K2Xd61r','ww4cf07f7d23045699')
+            ret = wxcpt.VerifyAESKey()
+            print ret
+
+            ret,sEchoStr=wxcpt.VerifyURL(signature, timestamp,nonce,echostr)
+
+            return sEchoStr
+
         except Exception, e:
             raise
         else:
@@ -18,32 +35,13 @@ class Handle(object):
         finally:
             pass
 
-'''
-    def GET(self):
-        try:
-            data = web.input()
-            if len(data) == 0:
-                return "hello, this is handle view"
-            signature = data.signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            token = "hello2018"  # 请按照公众平台官网\基本配置中信息填写
+    def AES_Decrypt(self, decoded_str):
+        key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+        iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
 
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            print
-            "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
-            else:
-                return ""
-        except Exception, Argument:
-            return Argument
-'''
+
+
+
     def POST(self):
         try:
             webData = web.data()
